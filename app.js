3f0c5b1b2818b744c2f1c79fd216303c9a522727
Max Liao <max.liao@gmail.com>
2018-03-24 14:05:21 -0400

    update

--- app.js
+++ app.js
@@ -12,80 +12,66 @@
 // Create a variable to reference the database
 var database = firebase.database();
 
+
 // Initial Values
 var name = "";
 var email = "";
 var age = 0;
 var comment = "";
-var members = 0;
+var startdate = "";
+var bill = 0;
+// var members = 0;
 
-database.ref().set({
-});
+
+// Clear Firebase
+// database.ref().set({
+
+//   });
 
 // At the initial load and subsequent value changes, get a snapshot of the stored data.
 // This function allows you to update your page in real-time when the firebase database changes.
 database.ref().on("value", function(snapshot) {
     console.log(snapshot.val());
-
-  // If Firebase has a highPrice and highBidder stored (first case)
-  if (snapshot.child("name").exists()) {
-   
-    // Set the variables to the stored values in firebase.
-    // name = snapshot.val().name;
-    // console.log(name);
-
-    // Print the data to the console.
-    // console.log(highBidder);
-    // console.log(highPrice);
-  }
-
-  // Else Firebase doesn't have anything so use the initial local values.
-  else {
-    // database.ref().update({
-    //     name: name,
-    //     email: email,
-    //     age: age,
-    //     comment: comment,
-    // });
-    }
 });
 
 database.ref().on("child_added", function(snapshot) {
     console.log("Child added");
-    members++;
-    // console.log(members);
-    // for ()
+    // members++;
+    // var index = members;
     var newname = snapshot.val().name;
     var newage = snapshot.val().age;
     var newemail = snapshot.val().email;
     var newcomment = snapshot.val().comment;
-    var index = members;
-
-    createRow(newname,newage,newemail,newcomment,index);
-
+    var newstartdate = snapshot.val().startdate;
+    var newmonthspassed = snapshot.val().months;
+    var newbill = snapshot.val().bill;
+    // console.log(newbill);
+    createRow(newname,newage,newemail,newcomment,newstartdate,newmonthspassed,newbill);
 });
 
-var createRow = function(newname,newage,newemail,newcomment,index) {
+var createRow = function(newname,newage,newemail,newcomment,newstartdate, newmonthspassed, newbill) {
     // Get reference to existing tbody element, create a new table row element
     var tBody = $("tbody");
     var tRow = $("<tr>");
 
     // Methods run on jQuery selectors return the selector they we run on
     // This is why we can create and save a reference to a td in the same statement we update its text
-    var membernum = $("<td>").text(index);
+    // var membernum = $("<td>").text(" " + index);
     var nameTd = $("<td>").text(newname);
     var ageTd = $("<td>").text(newage);
     var emailTd = $("<td>").text(newemail);
     var commentTd = $("<td>").text(newcomment);
+    var startdateTd = $("<td>").text(newstartdate);
+    var monthsTd = $("<td>").text(newmonthspassed);
+    var billedTd = $("<td>").text(newbill);
+
     // Append the newly created table data to the table row
-    tRow.append(index, nameTd, ageTd, emailTd, commentTd);
+    tRow.append(nameTd, ageTd, emailTd, commentTd, startdateTd, monthsTd, billedTd);
+    // tRow.append(nameTd, ageTd, emailTd, commentTd, startdateTd);
+
     // Append the table row to the table body
     tBody.append(tRow);
-    // console.log(tBody);
-    // $("#Table").append(tBody);
   };
-
-
 
 // This function handles events where the name button is clicked
 $("#submit").on("click", function(event) {
@@ -98,18 +84,44 @@ $("#submit").on("click", function(event) {
     email = $("#email-input").val().trim();
     age = $("#age-input").val().trim();
     comment = $("#comment-input").val().trim();
+    startdate = $("#startdate-input").val().trim();
+    if (startdate.length == 8){
+        // console.log("Correct length");
+        var year = startdate.substring(0,4);
+        var month = startdate.substring(4,6);
+        var day = startdate.substring(6,8);
+        // console.log(moment().format("YYYYMMDD"));
 
-    // if (name == ""){
-    //     console.log("Empty Name");
-    // } else if (email == "") {
-    //     console.log("Empty Email");
-    // } else if (age== "") {
-    //     console.log("Empty Age");
-    // } else if (comment == "") {
-    //     console.log("Empty Comment");
-    // } else {
-    //     console.log("All Fields Present");
-    // }
+        var currenttime = moment().format("YYYYMMDD");
+
+        var datemoment = moment(startdate);
+        console.log(datemoment);
+
+        var monthspassed = datemoment.from(currenttime,true);
+
+        var currentyear = currenttime.substring(0,4);
+        var currentmonth = currenttime.substring(4,6);
+        var currentday = currenttime.substring(6,8);
+
+        var yearbilled = 50000;
+        var monthbilled = yearbilled/12;
+        var daybilled = yearbilled/365;
+
+        var dyear = currentyear - year;
+        var dmonth = currentmonth - month;
+        var dday = currentday - day;
+
+        var billed = precisionRound((dyear*yearbilled + dmonth*monthbilled + dday*daybilled),2);
+        // console.log(precisionRound(billed, 2));
+        // console.log(billed);
+
+        bill = billed;
+
+        // console.log(moment(startdate, "YYYYMMDD").fromNow());
+    } else {
+        billed = "";
+        console.log("Incorrect length");
+    }
 
     // Save the new post in Firebase
     database.ref().push({
@@ -117,30 +129,14 @@ $("#submit").on("click", function(event) {
         email: email,
         age: age,
         comment: comment,
+        startdate: startdate,
+        months: monthspassed,
+        bill: billed,
       });
   
 });  
 
-//   function writeNewPost(uid, username, picture, title, body) {
-//     // A post entry.
-//     var postData = {
-//       author: username,
-//       uid: uid,
-//       body: body,
-//       title: title,
-//       starCount: 0,
-//       authorPic: picture
-//     };
-  
-//     // Get a key for a new Post.
-//     var newPostKey = firebase.database().ref().child('posts').push().key;
-  
-//     // Write the new post's data simultaneously in the posts list and the user's post list.
-//     var updates = {};
-//     updates['/posts/' + newPostKey] = postData;
-//     updates['/user-posts/' + uid + '/' + newPostKey] = postData;
-  
-//     return firebase.database().ref().update(updates);
-//   }
-
-//   console.log(writeNewPost(1, "Max", "pic", "title", "body"));
+function precisionRound(number, precision) {
+    var factor = Math.pow(10, precision);
+    return Math.round(number * factor) / factor;
+  }
