173c5fee16081604f14d746e58f8f7e7edaf67b5
Max Liao <max.liao@gmail.com>
2018-03-24 11:10:38 -0400

    firebase error

--- app.js
+++ app.js
@@ -1,26 +1,54 @@
 // Initialize Firebase
-// Make sure to match the configuration to the script version number in the HTML
-// (Ex. 3.0 != 3.7.0)
-// Initialize Firebase
 var config = {
-    apiKey: "AIzaSyCAQ69TwLNNWW4TRn0zYsQV2GL2Tio4kSA",
-    authDomain: "first-pro-a1a2e.firebaseapp.com",
-    databaseURL: "https://first-pro-a1a2e.firebaseio.com",
-    projectId: "first-pro-a1a2e",
-    storageBucket: "first-pro-a1a2e.appspot.com",
-    messagingSenderId: "486106310535"
-};
-firebase.initializeApp(config);
+    apiKey: "AIzaSyAXA-mKK0pKfEk5xngqYJesNJWs-CIBo70",
+    authDomain: "time-s.firebaseapp.com",
+    databaseURL: "https://time-s.firebaseio.com",
+    projectId: "time-s",
+    storageBucket: "time-s.appspot.com",
+    messagingSenderId: "763496059909"
+  };
+  firebase.initializeApp(config);
 
 // Create a variable to reference the database
 var database = firebase.database();
 
+// Initial Values
+var name = "";
+var email = "";
+var age = 0;
+var comment = "";
 
 // At the initial load and subsequent value changes, get a snapshot of the stored data.
 // This function allows you to update your page in real-time when the firebase database changes.
 database.ref().on("value", function(snapshot) {
     console.log(snapshot.val());
 });
+
+// This function handles events where the name button is clicked
+$("#submit").on("click", function(event) {
+    // event.preventDefault() prevents submit button from trying to send a form.
+    // Using a submit button instead of a regular button allows the user to hit
+    // "Enter" instead of clicking the button if desired
+    event.preventDefault();
+    
+    name = $("#name-input").val().trim();
+    email = $("#email-input").val().trim();
+    age = $("#age-input").val().trim();
+    comment = $("#comment-input").val().trim();
+    
+    console.log(name);
+
+    // var newtopic = $("#name-input").val().trim();
+    if (name == ""){
+        // var temp = typeof(newtopic);
+        console.log("Empty String");
+    } else {
+        console.log(name);
+        // topics.push(newtopic);
+    }
+    // The renderButtons function is called, rendering the list of gif buttons
+    // renderButtons();
+});  
 
   function writeNewPost(uid, username, picture, title, body) {
     // A post entry.
